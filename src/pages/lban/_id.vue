b2bdc5035e65718f2a0326e6cf7428efef4f29f0
Tomachi [ICHIGO] <lego@aol.jp>
2021-10-02 07:13:04 +0900

    feat: 各処罰ページ作成完了

--- src/pages/lban/_id.vue
+++ src/pages/lban/_id.vue
@@ -1,3 +1,125 @@
 <template>
-  <div>LBan 処罰情報詳細ページ</div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <h1>{{ banType }}#{{ banId }} - {{ detail.player }}</h1>
+
+        <v-container>
+          <v-carousel cycle height="auto">
+            <v-carousel-item
+              v-for="(proof, index) in proofs"
+              :key="index"
+              :src="`https://storage.jaoafa.com/${proof}`"
+            />
+          </v-carousel>
+        </v-container>
+
+        <v-simple-table>
+          <template #default>
+            <tbody>
+              <tr v-for="(key_ja, key) in keys" :key="key">
+                <td>
+                  {{ key_ja }}
+                  <code>({{ key }})</code>
+                </td>
+                <td v-if="key == 'banned_at' || key == 'updated_at'">
+                  {{ formatDate(new Date(detail[key]), 'yyyy/MM/dd HH:mm:ss') }}
+                </td>
+                <td v-else-if="key == 'status'">
+                  <code>{{ Boolean(detail[key]) }}</code>
+                </td>
+                <td v-else-if="key == 'player'">
+                  <a :href="`https://users.jaoafa.com/${detail['uuid']}`">
+                    {{ detail[key] }}
+                  </a>
+                </td>
+                <td v-else>
+                  <code>{{ detail[key] }}</code>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      banType: 'LBan',
+      banId: '',
+      keys: {
+        banid: '処罰番号',
+        player: 'MinecraftID',
+        uuid: 'MinecraftUUID',
+        reason: '処罰理由',
+        banned_by: '処罰者',
+        banned_at: '処罰日時',
+        updated_at: '情報更新日時',
+      },
+      detail: {},
+      proofs: [],
+    }
+  },
+  mounted() {
+    try {
+      this.$recaptcha.init()
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('reCAPTCHA Error', e)
+    }
+
+    this.banId = this.$route.params.id
+
+    this.$recaptcha.execute('login').then((token: string) => {
+      this.$axios
+        .get(
+          `https://api.jaoafa.com/v2/bans/details/${this.banType.toLowerCase()}/${
+            this.banId
+          }`,
+          {
+            headers: {
+              'X-reCAPTCHA-Token': token,
+            },
+          }
+        )
+        .then((response) => {
+          if (!response.data.status) {
+            alert('Not Found')
+            this.$router.push('/')
+            return
+          }
+          this.banId = response.data.detail.banid
+          this.detail = response.data.detail
+          this.proofs = response.data.proofs
+        })
+        .catch((error) => {
+          if (error.status === 404) {
+            alert('Not Found')
+            this.$router.push('/')
+            return
+          }
+          // eslint-disable-next-line no-console
+          console.error(error)
+        })
+    })
+  },
+  methods: {
+    formatDate(date: Date, format: string): string {
+      format = format.replace(/yyyy/g, String(date.getFullYear()))
+      format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
+      format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2))
+      format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2))
+      format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2))
+      format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2))
+      format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3))
+      return format
+    },
+  },
+})
+</script>
